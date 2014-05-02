651b2b400034b6689f36c7ffee68738d3cf010ea
Kashi <Kashi@130.229.174.224>
2014-05-02 11:58:50 +0200

    fixat ihop allt (mergeat typ)

--- Yatzy/src/yatzy/Game.java
+++ Yatzy/src/yatzy/Game.java
@@ -2,7 +2,6 @@ package yatzy;
 
 import java.awt.Color;
 import java.awt.Dimension;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,34 +9,34 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Game {
- 
-   	
-  public static void main(String args[]) {
-	  Dice[] dice = new Dice[5];
-	  SwingUtilities.invokeLater(new Runnable() {
-          public void run() {
-              createGameGUI();
-              
-          }
-  });
-	  
- }
-  
-  private static void createGameGUI(){
-      JFrame gameField = new JFrame("Yatzy");
-      GamePanel gameBackround = new GamePanel();
-      gameField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      gameField.setSize(500,500);
-      gameField.add(gameBackround);
-      gameBackround.add(new JButton("test"));
-      gameField.setVisible(true);
-      
-  }
+
+
+	public static void main(String args[]) {
+		Dice[] dice = new Dice[5];
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createGameGUI();
+
+			}
+		});
+
+	}
+
+	private static void createGameGUI(){
+		JFrame gameField = new JFrame("Yatzy");
+		GamePanel gameBackround = new GamePanel();
+		gameField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameField.setSize(850,600);
+		gameField.add(gameBackround);
+		gameBackround.add(new JButton("Tryck på mig ples"));
+		gameField.setVisible(true);
+
+	}
 }
 
 class GamePanel extends JPanel {
-	
+
 	public GamePanel() {
-        setBorder(BorderFactory.createLineBorder(Color.red));
+		setBorder(BorderFactory.createLineBorder(Color.red));
 	}
 }
