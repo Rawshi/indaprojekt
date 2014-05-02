bb6bd661f5e2173f7ade987efa1edab3800ae289
Egeras <Edvard.mickos@gmail.com>
2014-05-02 02:04:51 +0200

    Mer Stuff

--- Yatzy/src/yatzy/Game.java
+++ Yatzy/src/yatzy/Game.java
@@ -1,26 +1,32 @@
 package yatzy;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Game {
  
    	
   public static void main(String args[]) {
-	  if (args.length > 1){
-		  System.exit(1);
-	  }
-	  final int PLAYERAMOUNT = args[0];
+	  Dice[] dice = new Dice[5];
 	  SwingUtilities.invokeLater(new Runnable() {
           public void run() {
-              creatGameGUI(); 
+              createGameGUI(); 
           }
   });
 	  
-  }
+ }
+  
   private static void createGameGUI(){
       JFrame gameField = new JFrame("Yatzy");
       gameField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
   }
+}
+
+class GamePanel extends JPanel {
+	
+	public GamePanel() {
+		
+	}
 }
