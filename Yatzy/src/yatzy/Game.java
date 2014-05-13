ae8ec01ceb2f9cd0729ecaf87d91149c9a9ece26
egeras <edvard.mickos@gmail.com>
2014-05-13 08:48:23 +0200

    Intro

--- Yatzy/src/yatzy/Game.java
+++ Yatzy/src/yatzy/Game.java
@@ -10,37 +10,44 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Game {
-	
+
 	static Color gameColor = new Color(85, 140, 47);
 
 	public static void main(String args[]) {
-	      createGameGUI();
+		createGameGUI();
 
- 
 	}
 
+	private static void createGameGUI() {
+		// Custom button text
+		Object[] options = { "One Player", "Two Players", "More.." };
+		int players = JOptionPane.showOptionDialog(null,
+				"How many players are there?", "Yatzy",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+				null, options, options[2]);
 
-	private static void createGameGUI(){
-		int players = Integer.parseInt(JOptionPane.showInputDialog("Derp"))+1; //amount of player + 1 button row
+		if (players > 2) {
+			players = Integer
+					.parseInt(JOptionPane.showInputDialog("How many?"));
+		}
+		System.out.println(players);
 		JFrame gameField = new JFrame("Yatzy");
 		gameField.setResizable(false);
-		//JPanel gameBorder = new JPanel();
+		// JPanel gameBorder = new JPanel();
 		GamePanel gameBackground = new GamePanel();
-        Scoresheet gameBorder2 = new Scoresheet(players, gameBackground);
-        
-		gameField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gameField.setSize(400+(40*players),600);
+		Scoresheet gameBorder2 = new Scoresheet(players+2, gameBackground);
 
-		//gameBorder.setBackground(Color.green);
+		gameField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameField.setSize(400 + (40 * players), 600);
+
+		// gameBorder.setBackground(Color.green);
 		gameBorder2.setBackground(gameColor);
 		gameBackground.setBackground(Color.white);
-		gameBackground.
-		setBorder(BorderFactory.createLineBorder(gameColor, 7));
+		gameBackground.setBorder(BorderFactory.createLineBorder(gameColor, 7));
 
-		//gameField.add(gameBorder, BorderLayout.WEST);
+		// gameField.add(gameBorder, BorderLayout.WEST);
 		gameField.add(gameBorder2, BorderLayout.CENTER);
 		gameField.add(gameBackground, BorderLayout.WEST);
-         
 
 		gameField.setVisible(true);
 		gameBackground.repaint();
